ff86fb27c0abd8d3728cafc76033428b39f87271
Aq0amancer <predrag364@gmail.com>
2025-07-07 00:03:51 +0200

    Working version with Flux, no NSFW yet

--- slavica_main.py
+++ slavica_main.py
@@ -1,270 +1,234 @@
 import discord
 from discord.ext import commands
+import asyncio
 from discord import FFmpegPCMAudio
 import os
 import random
+import subprocess
 import configparser
 import yt_dlp
-import openai
+from openai import OpenAI
 import urllib
+from mflux import Flux1, Config
+import datetime
+import time
 
 # Read token################################
 config = configparser.ConfigParser()
 config.read_file(open(r'TOKEN.cfg'))
 TOKEN = config.get('TOKENS', 'TOKEN_1')
-openai.api_key=config.get('TOKENS', 'OPENAI_API_KEY')
 ############################################
+
+client = OpenAI(api_key=config.get('TOKENS', 'OPENAI_API_KEY'))
+
 # Radio URLS
-radio_urls={'tdi':'https://streaming.tdiradio.com/tdiradio.mp3','bbc':'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one'}
+radio_urls = {
+    'tdi': 'https://streaming.tdiradio.com/tdiradio.mp3',
+    'bbc': 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one'
+}
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+intents.messages = True
+intents.guilds = True
+intents.voice_states = True
+intents.guild_messages = True
 
-## Open AI
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-def generate_image(prompt):
-    response = openai.Image.create(
+# Load the model
+flux = Flux1.from_name(
+   model_name="dev",  # "schnell" or "dev"
+   quantize=8,            # 4 or 8
+)
+
+""" 
+def generate_flux_image(prompt):
+    image = flux.generate_image(
+    seed=2,
     prompt=prompt,
-    n=1,
-    size="256x256"
+    config=Config(
+        num_inference_steps=4,  # "schnell" works well with 2-4 steps, "dev" works well with 20-25 steps
+        height=1024,
+        width=1440,
+    ),
     )
-    return response['data'][0]['url']
+    return image
+"""
+def generate_flux_image(prompt, output_path):
+    # Prepare output path
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # Build the command
+    command = [
+        "mflux-generate",
+        "--prompt", prompt,
+        "--model", "dev",
+        "--steps", "14",
+        "--height", "768",
+        "--width", "1024",
+        "--seed", "1",
+        "-q", "8",
+        "--lora-paths", "lora/uberRealisticPornMerge_v13.safetensors",
+        "--output", output_path
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
 
 # Chat GPT text completion
 def generate_text(prompt):
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
     messages=[
         {"role": "user", "content": prompt}
-    ]
-    )
+    ])
     return completion.choices[0].message.content
 
-@ bot.event
+@bot.event
 async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-    print('We have logged in as a fucking {0.user}'.format(bot))
-
-
-@ bot.command(pass_context=True)
+@bot.command()
 async def join(ctx):
-
-    channel = ctx.author.voice.channel
-
-    await channel.connect()
-
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not connected to a voice channel.")
 
 @bot.command()
 async def test(ctx, arg):
-
     await ctx.send(arg)
 
 # PAINTED WORLD OF SLAVICA
 @bot.command()
-async def nacrtaj(ctx,prompt):
-    # Get AI generated image
-    image_url=generate_image(prompt)
-    image_path=f'/home/pi/Slavica/openai_images/{prompt}.jpg'
-    urllib.request.urlretrieve(image_url, image_path)
-    await ctx.send(file=discord.File(image_path))
+async def nacrtaj(ctx, prompt):
+    date_prefix = datetime.datetime.now().strftime("%Y%m%d")
+    rand_digits = random.randint(00, 99)
+    filename = f"{date_prefix}_{rand_digits}_{prompt[0:40]}.jpg"
+    os.makedirs('flux_images', exist_ok=True)
+    image_path = os.path.join('flux_images', filename)
+    generate_flux_image(prompt,image_path)
+    # Save and ensure file is written
+    #image.save(image_path, image_path)
+    # Wait briefly to ensure file system sync (optional, but helps on some systems)
+    await asyncio.sleep(1)    
+    if os.path.exists(image_path):
+        await ctx.send(file=discord.File(image_path))
+    else:
+        await ctx.send("Failed to generate image.")
 
 @bot.command()
-async def napisi(ctx,prompt):
-    # Get AI generated image
-    text_response=generate_text(prompt)
+async def napisi(ctx, prompt):
+    # Get AI generated text
+    text_response = generate_text(prompt)
     await ctx.send(text_response)
 
 @bot.command()
 async def cibe(ctx):
-
-    await ctx.voice_client.disconnect()
-
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
 @bot.command()
 async def clear(ctx, amount=5):
-
     await ctx.channel.purge(limit=amount)
-
 
 @bot.command()
 async def vine(ctx):
-
     vine_file_name = random.choice(os.listdir("Memes"))
-
+    await ctx.send(file=discord.File(f"Memes/{vine_file_name}"))
 
 @bot.command()
 async def vic(ctx):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
 
     driver = webdriver.Chrome('chromedriver')
-
     driver.get("https://vicevi.net/site/best")
-
-    vic_text = driver.find_element_by_xpath("//*[@class='col-lg-12']").text
-
-    web_url = 'https://vicevi.net/site/best?page=15'
-
+    vic_text = driver.find_element(By.XPATH, "//*[@class='col-lg-12']").text
+    await ctx.send(vic_text)
+    driver.quit()
 
 @bot.command()
 async def radio(ctx, url: str = random.choice(list(radio_urls.values()))):
-    channel = ctx.message.author.voice.channel
-    global player
-    try:
-        player = await channel.connect()
-    except:
-        pass
-    player.play(FFmpegPCMAudio(random.choice(list(radio_urls.values()))))
-
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        if not ctx.voice_client:
+            await channel.connect()
+        ctx.voice_client.play(FFmpegPCMAudio(url))
+    else:
+        await ctx.send("You are not connected to a voice channel.")
 
 @bot.command()
 async def ispovest(ctx):
-
     ispovest = random.choice(os.listdir("Ispovesti"))
     video_name = ispovest[:-4]
-    video_link = 'Ispovesti/' + ispovest
+    video_link = f'Ispovesti/{ispovest}'
 
-    # check where the message was typed
+    if ctx.author.voice:
+        if not ctx.voice_client:
+            channel = ctx.author.voice.channel
+            await channel.connect()
 
-    if ctx.voice_client is None:
-
-        channel = ctx.author.voice.channel
-
-        await channel.connect()
-
-    YDL_OPTIONS = {'format': 'bestaudio/best',
-
-                   'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-
-                   'restrictfilenames': True,
-
-                   'noplaylist': True,
-
-                   'nocheckcertificate': True,
-
-                   'ignoreerrors': False,
-
-                   'logtostderr': False,
-
-                   'quiet': True,
-
-                   'no_warnings': True,
-
-                   'default_search': 'auto',
-
-                   # bind to ipv4 since ipv6 addresses cause issues sometimes
-                   'source_address': '0.0.0.0'
-
-                   }
-
-    FFMPEG_OPTIONS = {'options': '-vn'}
-    # voice = get(bot.voice_client, guild=ctx.guild)
-
-    server = ctx.message.guild
-
-    voice_channel = server.voice_client
-
-    if not voice_channel.is_playing():
-
-        voice_channel.play(FFmpegPCMAudio(
-            source=video_link, **FFMPEG_OPTIONS))
-
-        voice_channel.is_playing()
-
+        FFMPEG_OPTIONS = {'options': '-vn'}
+        ctx.voice_client.play(FFmpegPCMAudio(source=video_link, **FFMPEG_OPTIONS))
         await ctx.send(video_name)
-
     else:
-
-        await ctx.send("Cekaj da zavrsim pricu")
-        return
+        await ctx.send("You are not connected to a voice channel.")
 
 @bot.command()
-async def play(ctx, url : str):
+async def play(ctx, url: str):
+    if ctx.author.voice:
+        if not ctx.voice_client:
+            channel = ctx.author.voice.channel
+            await channel.connect()
 
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'noplaylist': True,
+        }
 
-    if ctx.voice_client is None:
-
-        channel = ctx.author.voice.channel
-
-        await channel.connect()
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],'noplaylist' : 1,
-    }
-
-    server = ctx.message.guild
-
-    voice_channel = server.voice_client
-
-    if not voice_channel.is_playing():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
         for file in os.listdir("./"):
             if file.endswith(".mp3"):
                 os.rename(file, "song.mp3")
-        voice_channel.play(discord.FFmpegPCMAudio("song.mp3"))\
-
-        voice_channel.is_playing()
-
+        ctx.voice_client.play(discord.FFmpegPCMAudio("song.mp3"))
     else:
-
-        await ctx.send("Cekaj da se zavrsi play ili kucaj '!stop'.")
-
-    return
-    
+        await ctx.send("You are not connected to a voice channel.")
 
 @bot.command()
 async def leave(ctx):
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice_client.is_connected():
-        await voiceChannel.disconnect()
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
-
 @bot.command()
 async def pause(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        ctx.voice_client.pause()
     else:
         await ctx.send("Currently no audio is playing.")
 
-
 @bot.command()
 async def resume(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
+    if ctx.voice_client and ctx.voice_client.is_paused():
+        ctx.voice_client.resume()
     else:
         await ctx.send("The audio is not paused.")
 
-
 @bot.command()
 async def stop(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    voice.stop()
-
-'''@ bot.event
-
-async def on_message(message):
-
-    if message.author == bot.user:
-
-        return
-
-
-
-    if message.content.startswith('Hello'):
-
-        await message.channel.send('Hello!')
-
-        print('Message')
-
-'''
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+    else:
+        await ctx.send("No audio is currently playing.")
 
 bot.run(TOKEN)
